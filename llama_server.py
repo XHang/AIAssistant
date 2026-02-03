add3c3418020c828179a6cd46c18cf71e782b0b1
cxh <1083594261@qq.com>
2026-02-03 17:01:59 +0800

    feat: init

---
+++ llama_server.py
@@ -0,0 +1,41 @@
+import json
+import subprocess
+import time
+
+
+class LlamaServer:
+    def __init__(self, config_path="config.json"):
+        with open(config_path, "r", encoding="utf-8") as f:
+            self.config = json.load(f)
+
+        self.process = None
+
+    def start(self):
+        model = self.config["model"]
+        ctx = str(self.config["ctx_size"])
+        threads = str(self.config["threads"])
+        port = str(self.config["port"])
+
+        cmd = [
+            "llama-server.exe",
+            "--model", model,
+            "--ctx-size", ctx,
+            "--threads", threads,
+            "--port", port
+        ]
+
+        self.process = subprocess.Popen(
+            cmd,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+            creationflags=subprocess.CREATE_NO_WINDOW
+        )
+
+        time.sleep(2)
+
+    def stop(self):
+        if self.process:
+            try:
+                self.process.terminate()
+            except:
+                pass
